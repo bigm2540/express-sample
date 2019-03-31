dc6f59cfaae8dc9f267f999bcca7ed52ba94479e
Bigbunny <Bm.anusorn@gmail.com>
2019-03-31 10:38:40 +0700

    เพิ่มพาท

--- controller/testController.js
+++ controller/testController.js
@@ -8,17 +8,41 @@ let responseObject = {
     result: ""
 }
 
-router.get("/getData",  (req, res) => {
+router.get("/getData", (req, res) => {
     let result = req.query.key1 * req.query.key2;
     res.send({ result: result })
 });
+// คำนวณสามเหลี่ยม
+router.get("/getTriangle", (req, res) => {
+    let result = 0.5 * req.query.base * req.query.height;
+    res.send({ Area: result })
+});
+// คำนวณBMI
+router.get("/getBMI", (req, res) => {
+    let BMI = Number(req.query.weight / ((req.query.height / 100) * (req.query.height / 100))).toFixed(2);
+    let msg = "";
+    if (BMI < 50) {
+        msg = "ผอม";
+    } else if (BMI > 50) {
+        msg = "อ้วน";
+    }
+    res.send({
+        BMI: BMI,
+        Status: msg
+    })
+});
+
 /*
 Request
 * */
-router.post("/postData",async  (req, res) => {
-    if(!req.body.key5){
+// if(value) value='',value=null,value=undefined return false  
+router.post("/postData", async (req, res) => {
+    responseObject={
+    msg:"",
+    result:""}
+    if (!req.body.key) {
         responseObject.msg = "Bad request";
-    }else{
+    } else {
         responseObject.msg = "success";
         responseObject.result = req.body.key * 5;
     }
